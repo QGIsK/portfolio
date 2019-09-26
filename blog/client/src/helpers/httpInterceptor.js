f0ff7093041fa98c0c3de5a6f6a168be22f590ce
Demian <Devaccdemiann@gmail.com>
2019-09-26 13:21:24 +0200

    Deleted blog for now

--- blog/client/src/helpers/httpInterceptor.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-import axios from 'axios';
-
-export default function execute() {
-    axios.interceptors.request.use(function(response) {
-        console.log(response)
-    });
-}
