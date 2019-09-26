167d336d0bf8e228122f16d34716fe700b8e17c3
Demian <Devaccdemiann@gmail.com>
2019-09-26 16:22:57 +0200

    temp disabled add urls

--- to/routes/index.js
+++ to/routes/index.js
@@ -37,58 +37,58 @@ router
         } catch (err) {
             res.status(500).json("Server error");
         }
-    })
-
-    /**
-     * @route   POST /shorten
-     * @desc    Create short url
-     */
-
-    .post(bruteforce.prevent, async (req, res) => {
-        const { longUrl, shortUrl } = req.body;
-
-        // Check if base url is valid
-        if (!validUrl.isUri(`${req.protocol}://${req.get("host")}`)) {
-            return res.status(401).json("Invalid base url");
-        }
-
-        // Create Url code
-        const urlCode = !shortUrl ? shortid.generate() : shortUrl;
-
-        // Check if longurl is valid
-        if (validUrl.isUri(longUrl)) {
-            try {
-                // Check if url exists in database
-                let url = await Url.findOne({
-                    longUrl
-                });
-
-                if (url) {
-                    return res.json(url);
-                }
-
-                // Make short url
-                const shortUrl = `${req.protocol}://${req.get(
-                    "host"
-                )}/${urlCode}`;
-
-                url = new Url({
-                    longUrl,
-                    shortUrl,
-                    urlCode,
-                    date: new Date()
-                });
-
-                // Save db
-                await url.save();
-
-                return res.json(url);
-            } catch (err) {
-                res.status(500).json("Server error");
-            }
-        } else {
-            res.status(401).json("Invalid long url");
-        }
     });
+
+/**
+ * @route   POST /shorten
+ * @desc    Create short url
+ */
+
+// .post(bruteforce.prevent, async (req, res) => {
+//     const { longUrl, shortUrl } = req.body;
+
+//     // Check if base url is valid
+//     if (!validUrl.isUri(`${req.protocol}://${req.get("host")}`)) {
+//         return res.status(401).json("Invalid base url");
+//     }
+
+//     // Create Url code
+//     const urlCode = !shortUrl ? shortid.generate() : shortUrl;
+
+//     // Check if longurl is valid
+//     if (validUrl.isUri(longUrl)) {
+//         try {
+//             // Check if url exists in database
+//             let url = await Url.findOne({
+//                 longUrl
+//             });
+
+//             if (url) {
+//                 return res.json(url);
+//             }
+
+//             // Make short url
+//             const shortUrl = `${req.protocol}://${req.get(
+//                 "host"
+//             )}/${urlCode}`;
+
+//             url = new Url({
+//                 longUrl,
+//                 shortUrl,
+//                 urlCode,
+//                 date: new Date()
+//             });
+
+//             // Save db
+//             await url.save();
+
+//             return res.json(url);
+//         } catch (err) {
+//             res.status(500).json("Server error");
+//         }
+//     } else {
+//         res.status(401).json("Invalid long url");
+//     }
+// });
 
 module.exports = router;
