ed529bbcfd238040b7cf2bb9ffa71bc7769e5512
Demian <Devaccdemiann@gmail.com>
2019-09-26 16:15:14 +0200

    Added ports

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,8 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET,
-  mongoUri: process.env.MONGO_URI
+    jwtSecret: process.env.JWT_SECRET,
+    mongoUri: process.env.MONGO_URI,
+    ports: {
+        portfolio: 3000,
+        to: 3001
+    }
 };
