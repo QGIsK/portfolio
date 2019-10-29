74a69e4df9a4d36a224be6023ee4e14700655c17
Demian <devaccdemiann@gmail.com>
2019-10-29 16:02:09 +0100

    Updated

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
-const connectDB = require("../db/");
-const config = require("../config");
+const connectDB = require("./db/");
+const config = require("./config");
 
 const app = express();
 
