9057e563a00222a90b337b7ef73b0d57192de128
Demian <devaccdemiann@gmail.com>
2019-10-29 20:19:10 +0100

    Added try catch return 500

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,29 +3,33 @@ const Mail = require("../helpers/mail");
 const Config = require("../config/");
 
 exports.store = async (req, res) => {
-    const from = req.sanitize(req.body.from);
-    const email = req.sanitize(req.body.email);
-    const subject = req.sanitize(req.body.subject);
-    const body = req.sanitize(req.body.body);
+    try {
+        const from = req.sanitize(req.body.from);
+        const email = req.sanitize(req.body.email);
+        const subject = req.sanitize(req.body.subject);
+        const body = req.sanitize(req.body.body);
 
-    if (!from || !email || !subject || !body)
-        return res.status(422).json({ error: "Please provide all fields" });
+        if (!from || !email || !subject || !body)
+            return res.status(422).json({ error: "Please provide all fields" });
 
-    const mailOptions = {
-        from,
-        to: Config.mail,
-        subject,
-        text: body
-    };
+        const mailOptions = {
+            from,
+            to: Config.mail,
+            subject,
+            text: body
+        };
 
-    await Mail.send(mailOptions);
+        await Mail.send(mailOptions);
 
-    new Contact({
-        from,
-        email,
-        subject,
-        body
-    }).save();
+        new Contact({
+            from,
+            email,
+            subject,
+            body
+        }).save();
 
-    res.status(200).json({ msg: "Email has been send!" });
+        res.status(200).json({ msg: "Email has been send!" });
+    } catch (err) {
+        res.status(500).json("Server error");
+    }
 };
