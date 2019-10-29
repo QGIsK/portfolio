61f5af8cdff561c0ed4c3096214ec8b73b8d5d4f
Demian <devaccdemiann@gmail.com>
2019-10-29 16:07:06 +0100

    Removed express brute

--- routes/redirect.js
+++ routes/redirect.js
@@ -1,21 +1,10 @@
 const express = require("express");
 const validUrl = require("valid-url");
 const shortid = require("shortid");
-const ExpressBrute = require("express-brute");
 
 const router = express.Router();
 
 const Url = require("../db/models/url");
-
-if (!process.env.environment || process.env.environment === "development") {
-    store = new ExpressBrute.MemoryStore();
-} else {
-    store = new MemcachedStore(["127.0.0.1"], {
-        prefix: "NoConflicts"
-    });
-}
-
-const bruteforce = new ExpressBrute(store);
 
 router
     .route("/:code")
