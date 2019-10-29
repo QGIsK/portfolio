8c2c8fa0e584f70418bc02ef9d1324fbeb6d9627
Demian <devaccdemiann@gmail.com>
2019-10-29 20:18:11 +0100

    Added sanitizer

---
+++ controllers/contactController.js
@@ -0,0 +1,31 @@
+const Contact = require("../db/models/contact");
+const Mail = require("../helpers/mail");
+const Config = require("../config/");
+
+exports.store = async (req, res) => {
+    const from = req.sanitize(req.body.from);
+    const email = req.sanitize(req.body.email);
+    const subject = req.sanitize(req.body.subject);
+    const body = req.sanitize(req.body.body);
+
+    if (!from || !email || !subject || !body)
+        return res.status(422).json({ error: "Please provide all fields" });
+
+    const mailOptions = {
+        from,
+        to: Config.mail,
+        subject,
+        text: body
+    };
+
+    await Mail.send(mailOptions);
+
+    new Contact({
+        from,
+        email,
+        subject,
+        body
+    }).save();
+
+    res.status(200).json({ msg: "Email has been send!" });
+};
