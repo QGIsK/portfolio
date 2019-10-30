74decea0aee10bbe7ca3e635a9043b21d0d28ebf
Demian <Devaccdemiann@gmail.com>
2019-10-30 11:27:31 +0100

    Refactored

--- client/src/main.js
+++ client/src/main.js
@@ -11,27 +11,22 @@ import "vuetify/dist/vuetify.min.css";
 
 Vue.use(Vuetify);
 
-const token = localStorage.getItem("token");
-
 Vue.config.productionTip = false;
 Vue.prototype.$http = Axios;
-Vue.prototype.$http.defaults.headers.common["Content-Type"] = "application/json";
+Vue.prototype.$http.defaults.headers.common["Content-Type"] =
+    "application/json";
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV == "development") {
-  Vue.prototype.$http.defaults.baseURL = "http://localhost:3000";
-}
-
-if (token) {
-  Vue.prototype.$http.defaults.headers.common["Authorization"] = token;
+    Vue.prototype.$http.defaults.baseURL = "http://localhost:3000";
 }
 
 /* eslint-disable no-new */
 new Vue({
-  el: "#app",
-  router,
-  store,
-  components: {
-    App,
-  },
-  template: "<App/>",
+    el: "#app",
+    router,
+    store,
+    components: {
+        App
+    },
+    template: "<App/>"
 });
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -4,134 +4,14 @@ import Vuex from "vuex";
 import axios from "axios";
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV == "development") {
-  axios.defaults.baseURL = "http://localhost:3000";
+    axios.defaults.baseURL = "http://localhost:3000";
 }
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    status: "",
-    token: localStorage.getItem("token") || "",
-    user: JSON.parse(localStorage.getItem("user")) || {},
-  },
-  mutations: {
-    auth_request(state) {
-      state.status = "loading";
-    },
-    auth_success(state, { token, user }) {
-      state.status = "success";
-      state.token = token;
-      state.user = user;
-    },
-    auth_error(state) {
-      state.status = "error";
-    },
-    logout(state) {
-      state.status = "";
-      state.token = "";
-      state.user = {};
-    },
-  },
-  actions: {
-    login({ commit }, user) {
-      return new Promise((resolve, reject) => {
-        commit("auth_request");
-        axios({
-          url: "/api/auth/login",
-          data: {
-            email: user.email,
-            password: user.password,
-          },
-          crossdomain: true,
-          method: "POST",
-        })
-          .then(resp => {
-            const token = resp.data.token;
-            const user = {
-              id: resp.data.user._id,
-            };
-
-            let payload = {
-              type: "success",
-              text: "Successfully logged in",
-            };
-
-            localStorage.setItem("token", token);
-            localStorage.setItem("user", JSON.stringify(user));
-            axios.defaults.headers.common["Authorization"] = token;
-            commit("toggleSnackBar", payload);
-            commit("auth_success", {
-              token,
-              user,
-            });
-            resolve(resp);
-          })
-          .catch(err => {
-            commit("auth_error", err);
-            localStorage.removeItem("token");
-            reject(err);
-          });
-      });
-    },
-    register({ commit }, user) {
-      return new Promise((resolve, reject) => {
-        commit("auth_request");
-        axios({
-          url: "/api/auth/register",
-          crossdomain: true,
-          data: user,
-          method: "POST",
-        })
-          .then(resp => {
-            const token = resp.data.token;
-            const user = {
-              id: resp.data.user._id,
-            };
-
-            let payload = {
-              type: "success",
-              text: "Successfully registered",
-            };
-
-            localStorage.setItem("token", token);
-            localStorage.setItem("user", JSON.stringify(user));
-
-            axios.defaults.headers.common["Authorization"] = token;
-            commit("auth_success", {
-              token,
-              user,
-            });
-            commit("toggleSnackBar", payload);
-            resolve(resp);
-          })
-          .catch(err => {
-            commit("auth_error", err);
-            localStorage.removeItem("user");
-            localStorage.removeItem("token");
-            reject(err);
-          });
-      });
-    },
-    logout({ commit }) {
-      return new Promise((resolve, reject) => {
-        let payload = {
-          type: "success",
-          text: "Successfully logged out",
-        };
-
-        localStorage.removeItem("token");
-        localStorage.removeItem("user");
-        delete axios.defaults.headers.common["Authorization"];
-        commit("toggleSnackBar", payload);
-        commit("logout");
-        resolve();
-      });
-    },
-  },
-  getters: {
-    isLoggedIn: state => !!state.token,
-    userId: state => state.user.id,
-    authStatus: state => state.status,
-  },
+    state: {},
+    mutations: {},
+    actions: {},
+    getters: {}
 });
