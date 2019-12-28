36ebdc693a1abdae9c27513c81dcd18439fb2a7a
Demian <devaccdemiann@gmail.com>
2019-12-29 00:17:46 +0100

    Changed limiter

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,18 +8,16 @@ const rateLimit = require("express-rate-limit");
 
 const normalLimiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 75, // limit each IP to 100 requests per windowMs
+  max: 75,
 });
 
 const contactLimiter = rateLimit({
   windowMs: 60 * 60 * 1000, // 60 minutes
-  max: 5, // limit each IP to 100 requests per windowMs
+  max: 2,
 });
 
-router.use(normalLimiter);
-
-router.get("/r/:code", redirectController.index);
 // router.post("/shorten", redirectController.store);
+router.get("/r/:code", normalLimiter, redirectController.index);
 router.post("/contact", contactLimiter, contactController.store);
 
 module.exports = router;
