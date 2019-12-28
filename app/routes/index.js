fd301ccb9d955fe379b69a7432e7100f82c7c491
Demian <devaccdemiann@gmail.com>
2019-12-28 21:53:25 +0100

    Added rate limiters and email

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,19 +2,24 @@ const express = require("express");
 const router = express.Router();
 
 const redirectController = require("../controllers/RedirectController");
-// const contactController = require("../controllers/ContactController");
+const contactController = require("../controllers/ContactController");
 
 const rateLimit = require("express-rate-limit");
 
 const normalLimiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 75, // limit each IP to 100 requests per windowMs
+});
+
+const contactLimiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 60 minutes
+  max: 5, // limit each IP to 100 requests per windowMs
 });
 
 router.use(normalLimiter);
 
 router.get("/r/:code", redirectController.index);
 // router.post("/shorten", redirectController.store);
-// router.post("/contact", contactController.store);
+router.post("/contact", contactLimiter, contactController.store);
 
 module.exports = router;
