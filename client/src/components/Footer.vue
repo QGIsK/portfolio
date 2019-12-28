e6a58e575d207ac360921fe543d1206271448616
Demian <devaccdemiann@gmail.com>
2019-12-28 21:54:09 +0100

    added bg dark

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,27 +1,29 @@
 <template>
-  <v-footer color="transparent darken-1" padless>
-    <v-row justify="center" no-gutters>
-      <v-btn
-        color="white"
-        text
-        rounded
-        class="my-2"
-        target="_blank"
-        href="https://github.com/qgisk"
-      >Github</v-btn>
-      <v-btn color="white" text rounded class="my-2" target="_blank" href="#">Discord</v-btn>
-      <v-btn color="white" text rounded class="my-2" target="_blank" href="#">Instagram</v-btn>
-      <v-btn color="white" text rounded class="my-2" target="_blank" href="#">Telegram</v-btn>
-      <v-btn color="white" text rounded class="my-2" target="_blank" href="#">Linkedin</v-btn>
-      <v-col class="lighten-2 py-4 text-center white--text" cols="12">
-        <strong>
-          Portfolio
-          &copy;
-          {{ new Date().getFullYear() }}
-        </strong>
-      </v-col>
-    </v-row>
-  </v-footer>
+  <div class="bg-dark">
+    <v-footer style="z-index: 10;" color="transparent darken-1" padless>
+      <v-row justify="center" no-gutters>
+        <v-btn
+          color="white"
+          text
+          rounded
+          class="my-2"
+          target="_blank"
+          href="https://github.com/qgisk"
+        >Github</v-btn>
+        <v-btn color="white" text rounded class="my-2" target="_blank" href="#">Discord</v-btn>
+        <v-btn color="white" text rounded class="my-2" target="_blank" href="#">Instagram</v-btn>
+        <v-btn color="white" text rounded class="my-2" target="_blank" href="#">Telegram</v-btn>
+        <v-btn color="white" text rounded class="my-2" target="_blank" href="#">Linkedin</v-btn>
+        <v-col class="lighten-2 py-4 text-center white--text" cols="12">
+          <strong>
+            Portfolio
+            &copy;
+            {{ new Date().getFullYear() }}
+          </strong>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </div>
 </template>
 
 <script>
