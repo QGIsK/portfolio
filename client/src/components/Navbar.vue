4e15ad637930c7e325250908388e59e19a14a9c7
Demian <devaccdemiann@gmail.com>
2020-01-01 16:38:45 +0100

    Added about llink

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -3,12 +3,12 @@
     <v-toolbar flat style="cursor:pointer; background: transparent; z-index:10;">
       <!-- <v-app-bar-nav-icon class="ml-2"></v-app-bar-nav-icon> -->
 
-      <v-toolbar-title @click="redirect('/');" style="margin-left:7.5vh">Demiann</v-toolbar-title>
+      <v-toolbar-title @click="redirect('/')" style="margin-left:7.5vh">Demiann</v-toolbar-title>
 
       <v-spacer></v-spacer>
       <v-toolbar-items style="margin-right:7.5vh; z-index:10">
         <v-btn text rounded v-scroll-to="'#portfolio'" style="z-index:10">Home</v-btn>
-        <v-btn text rounded disabled style="z-index:10">About</v-btn>
+        <v-btn text rounded v-scroll-to="'#footer'" style="z-index:10">About</v-btn>
         <v-btn text rounded v-scroll-to="'#footer'" style="z-index:10">Contact</v-btn>
       </v-toolbar-items>
     </v-toolbar>
