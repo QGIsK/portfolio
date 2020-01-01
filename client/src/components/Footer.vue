8034d2bc34c585ce5c73d607a2d368931f5d978d
Demian <devaccdemiann@gmail.com>
2020-01-01 16:38:30 +0100

    Added top padding

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="footer" class="bg-dark">
-    <v-footer color="transparent darken-1" padless>
-      <v-row justify="center" no-gutters>
+    <v-footer class="mt-3" color="transparent" padless>
+      <v-row justify="center" no-gutters class="mt-4">
         <v-btn
           color="white"
           text
@@ -10,47 +10,23 @@
           rel="noreferrer"
           target="_blank"
           href="https://github.com/qgisk"
-        >Github</v-btn>
-        <v-btn
-          color="white"
-          rel="noreferrer"
-          text
-          rounded
-          class="my-2"
-          target="_blank"
-          href="#"
-        >Discord</v-btn>
-        <v-btn
-          color="white"
-          rel="noreferrer"
-          text
-          rounded
-          class="my-2"
-          target="_blank"
-          href="#"
-        >Instagram</v-btn>
-        <v-btn
-          color="white"
-          rel="noreferrer"
-          text
-          rounded
-          class="my-2"
-          target="_blank"
-          href="#"
-        >Telegram</v-btn>
-        <v-btn
-          color="white"
-          rel="noreferrer"
-          text
-          rounded
-          class="my-2"
-          target="_blank"
-          href="#"
-        >Linkedin</v-btn>
+          >Github</v-btn
+        >
+        <v-btn color="white" rel="noreferrer" text rounded class="my-2" target="_blank" href="#"
+          >Discord</v-btn
+        >
+        <v-btn color="white" rel="noreferrer" text rounded class="my-2" target="_blank" href="#"
+          >Instagram</v-btn
+        >
+        <v-btn color="white" rel="noreferrer" text rounded class="my-2" target="_blank" href="#"
+          >Telegram</v-btn
+        >
+        <v-btn color="white" rel="noreferrer" text rounded class="my-2" target="_blank" href="#"
+          >Linkedin</v-btn
+        >
         <v-col class="lighten-2 py-4 text-center white--text" cols="12">
           <strong>
-            Demiann
-            &copy;
+            Demiann &copy;
             {{ new Date().getFullYear() }}
           </strong>
         </v-col>
