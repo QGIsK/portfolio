7c734f7d6d25bc42b74fd80f4f5e31f910ed56f1
Demian <devaccdemiann@gmail.com>
2020-01-02 20:43:54 +0100

    Added more margin to social links

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -6,25 +6,53 @@
           color="white"
           text
           rounded
-          class="my-2"
+          class="my-2 mr-1"
           rel="noreferrer"
           target="_blank"
           href="https://github.com/qgisk"
           >Github</v-btn
         >
-        <v-btn color="white" rel="noreferrer" text rounded class="my-2" target="_blank" href="#"
+        <v-btn
+          color="white"
+          rel="noreferrer"
+          text
+          rounded
+          class="my-2 mx-1"
+          target="_blank"
+          href="#"
           >Discord</v-btn
         >
-        <v-btn color="white" rel="noreferrer" text rounded class="my-2" target="_blank" href="#"
+        <v-btn
+          color="white"
+          rel="noreferrer"
+          text
+          rounded
+          class="my-2 mx-1"
+          target="_blank"
+          href="#"
           >Instagram</v-btn
         >
-        <v-btn color="white" rel="noreferrer" text rounded class="my-2" target="_blank" href="#"
+        <v-btn
+          color="white"
+          rel="noreferrer"
+          text
+          rounded
+          class="my-2 mx-1"
+          target="_blank"
+          href="#"
           >Telegram</v-btn
         >
-        <v-btn color="white" rel="noreferrer" text rounded class="my-2" target="_blank" href="#"
+        <v-btn
+          color="white"
+          rel="noreferrer"
+          text
+          rounded
+          class="my-2 mx-1"
+          target="_blank"
+          href="#"
           >Linkedin</v-btn
         >
-        <v-col class="lighten-2 py-4 text-center white--text" cols="12">
+        <v-col class="lighten-2 my-3 py-4 text-center white--text" cols="12">
           <strong>
             Demiann &copy;
             {{ new Date().getFullYear() }}
