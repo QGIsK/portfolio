28257712be3be0d49bcdf6805cc5c53ef2d8e457
Demian <devaccdemiann@gmail.com>
2020-01-03 00:44:54 +0100

    Made toolbar bigger

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -3,7 +3,9 @@
     <v-toolbar flat style="cursor:pointer; background: transparent; z-index:10;">
       <!-- <v-app-bar-nav-icon class="ml-2"></v-app-bar-nav-icon> -->
 
-      <v-toolbar-title @click="redirect('/')" style="margin-left:7.5vh">Demiann</v-toolbar-title>
+      <v-toolbar-title @click="redirect('/')" style="margin-left:7.5vh" class="headline"
+        >Demiann</v-toolbar-title
+      >
 
       <v-spacer></v-spacer>
       <v-toolbar-items style="margin-right:7.5vh; z-index:10">
