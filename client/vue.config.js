81963ab8a0525484c813415c2f685c9526c22491
Demian <devaccdemiann@gmail.com>
2020-01-05 18:37:29 +0100

    REbuild

--- client/vue.config.js
+++ client/vue.config.js
@@ -10,5 +10,8 @@ if (process.env.NODE_ENV === "production") {
   module.exports = {
     transpileDependencies: ["vuetify"],
     outputDir: path.resolve(__dirname, "../resources/views/"),
+    devServer: {
+      proxy: "http://localhost:3000",
+    },
   };
 }
