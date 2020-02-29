d111dc0a846dd5b6642cf3d56625450922b77a89
Demian <devaccdemiann@gmail.com>
2020-02-29 15:01:07 +0100

    Changed discord tag

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -10,7 +10,8 @@
           rel="noreferrer"
           target="_blank"
           href="https://github.com/qgisk"
-        >Github</v-btn>
+          >Github</v-btn
+        >
         <v-hover v-slot:default="{ hover }" close-delay="175" open-delay="175">
           <v-btn
             color="white"
@@ -22,11 +23,9 @@
             href="#"
           >
             <v-expand-transition>
-              <div
-                v-if="hover"
-                class="d-flex darken-2 white--text"
-                style="height: 100%;"
-              >Demian#5769</div>
+              <div v-if="hover" class="d-flex darken-2 white--text" style="height: 100%;">
+                Demian#6666
+              </div>
               <div v-else>Discord</div>
             </v-expand-transition>
           </v-btn>
@@ -39,7 +38,8 @@
           class="my-2 mx-1"
           target="_blank"
           href="#"
-        >Instagram</v-btn>
+          >Instagram</v-btn
+        >
         <v-btn
           color="white"
           rel="noreferrer"
@@ -48,7 +48,8 @@
           class="my-2 mx-1"
           target="_blank"
           href="#"
-        >Telegram</v-btn>
+          >Telegram</v-btn
+        >
         <v-btn
           color="white"
           rel="noreferrer"
@@ -57,7 +58,8 @@
           class="my-2 mx-1"
           target="_blank"
           href="#"
-        >Linkedin</v-btn>
+          >Linkedin</v-btn
+        >
         <v-col class="lighten-2 my-3 py-4 text-center white--text" cols="12">
           <strong>
             Demiann &copy;
