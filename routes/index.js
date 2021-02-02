90e7d64391c59d63e317dd6982cae372aa06e912
Demian <devaccdemiann@gmail.com>
2021-02-02 18:10:28 +0100

    Get rid of api

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { Contact, Limiter } = require("./helpers/Limiters");
-const Controller = require("./controllers/ContactController");
-
-router.post("/contact", Contact, Controller.store);
-
-module.exports = router;
