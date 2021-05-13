d5f4325b59952a5d897616c816ba75e874bcb182
Demian <devaccdemiann@gmail.com>
2021-05-13 18:04:57 +0200

    Refactor

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,14 @@
 <template>
-  <div style="z-index:10">
-    <v-toolbar flat style="cursor:pointer; background: transparent; z-index:10;">
+  <div style="z-index: 10">
+    <v-toolbar flat style="cursor: pointer; background: transparent; z-index: 10">
       <!-- <v-app-bar-nav-icon class="ml-2"></v-app-bar-nav-icon> -->
 
-      <v-toolbar-title @click="redirect('/')" style="margin-left:7.5vh" class="headline"
+      <v-toolbar-title @click="redirect('/')" style="margin-left: 7.5vh" class="headline"
         >Demiann</v-toolbar-title
       >
 
       <v-spacer></v-spacer>
-      <v-toolbar-items style="margin-right:7.5vh;">
+      <v-toolbar-items style="margin-right: 7.5vh">
         <v-btn text rounded v-scroll-to="'#portfolio'">Home</v-btn>
         <v-btn text rounded v-scroll-to="'#footer'">Contact</v-btn>
       </v-toolbar-items>
@@ -21,9 +21,7 @@ export default {
   name: "Navbar",
   methods: {
     redirect(path) {
-      this.$router.push(path).catch(() => {
-        return;
-      });
+      this.$router.push(path);
     },
   },
 };
