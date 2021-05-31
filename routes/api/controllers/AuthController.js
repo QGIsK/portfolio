33594bada0dbc1ccde1dfc2ea1d05fc2a19ff405
Demian <Devaccdemiann@gmail.com>
2021-05-31 19:53:30 +0200

    msg -> message

--- routes/api/controllers/AuthController.js
+++ routes/api/controllers/AuthController.js
@@ -7,20 +7,20 @@ exports.login = async (req, res) => {
   let { email, password } = req.body;
   try {
     if (!password || !email)
-      return res.status(403).json({ msg: "Please fill in all fields correctly." });
+      return res.status(403).json({ message: "Please fill in all fields correctly." });
 
     const foundAdmin = await DB.Admin.findOne({
       email: email.toLowerCase(),
     });
 
     if (!foundAdmin) {
-      return res.status(403).json({ msg: "Unauthorized." });
+      return res.status(403).json({ message: "Unauthorized." });
     }
 
     const auth = await foundAdmin.comparePassword(password);
 
     if (!auth) {
-      return res.status(403).json({ msg: "Unauthorized." });
+      return res.status(403).json({ message: "Unauthorized." });
     }
 
     let token = await jwt.sign(
