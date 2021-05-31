8feefdf65005d876a8fafc94f5c4090cf46d51cd
Demian <Devaccdemiann@gmail.com>
2021-05-31 19:36:35 +0200

    Fix auth controller

--- routes/api/controllers/AuthController.js
+++ routes/api/controllers/AuthController.js
@@ -1,4 +1,7 @@
 const DB = require("@database");
+const jwt = require("jsonwebtoken");
+
+const { JWT_SECRET } = process.env;
 
 exports.login = async (req, res) => {
   let { email, password } = req.body;
@@ -6,15 +9,15 @@ exports.login = async (req, res) => {
     if (!password || !email)
       return res.status(403).json({ msg: "Please fill in all fields correctly." });
 
-    const foundUser = await DB.User.findOne({
+    const foundAdmin = await DB.Admin.findOne({
       email: email.toLowerCase(),
     });
 
-    if (!foundUser) {
+    if (!foundAdmin) {
       return res.status(403).json({ msg: "Unauthorized." });
     }
 
-    const auth = await foundUser.comparePassword(password);
+    const auth = await foundAdmin.comparePassword(password);
 
     if (!auth) {
       return res.status(403).json({ msg: "Unauthorized." });
@@ -22,21 +25,22 @@ exports.login = async (req, res) => {
 
     let token = await jwt.sign(
       {
-        id: foundUser._id,
+        id: foundAdmin._id,
       },
-      secret,
+      JWT_SECRET,
       {
         expiresIn: "1d",
       }
     );
 
-    const user = await DB.User.findById(foundUser.id).select(select.user);
+    const admin = await DB.Admin.findById(foundAdmin.id).select("name email");
 
     res.status(200).json({
       token,
-      user,
+      admin,
     });
   } catch (err) {
-    next(err);
+    console.log(err);
+    res.status(500).json("Server error");
   }
 };
