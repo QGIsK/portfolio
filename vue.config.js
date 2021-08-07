ffdc69fc3c6abc440350a618b263212d852c61de
Demian <devaccdemiann@gmail.com>
2021-08-07 23:00:59 +0200

    Update eslint prettier and packages

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,25 @@
-const marked = require("marked");
+const marked = require('marked');
 const renderer = new marked.Renderer();
 
 module.exports = {
-  transpileDependencies: ["vuetify"],
-  chainWebpack: config => {
+  transpileDependencies: ['vuetify'],
+  chainWebpack: (config) => {
     config.module
-      .rule("md")
+      .rule('md')
       .test(/\.md/)
-      .use("html-loader")
-      .loader("html-loader")
+      .use('html-loader')
+      .loader('html-loader')
       .end()
-      .use("markdown-loader")
-      .loader("markdown-loader")
-      .tap(options => {
+      .use('markdown-loader')
+      .loader('markdown-loader')
+      .tap((options) => {
         options = { pedantic: true, renderer };
 
         return options;
       })
       .end();
-    config.plugin("html").tap(args => {
-      args[0].title = "Demiann || Portfolio";
+    config.plugin('html').tap((args) => {
+      args[0].title = 'Demiann || Portfolio';
       return args;
     });
   },
