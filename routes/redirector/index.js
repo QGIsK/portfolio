4c0e5af2d130e19fac645796eccf3c91083058d6
Demian <devaccdemiann@gmail.com>
2021-08-08 18:08:35 +0200

    Create-nodejs-app init

--- routes/redirector/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const Controller = require('./controllers/RedirectController');
-
-router.get('/:code', Controller.index);
-
-module.exports = router;
---
+++ src/routes/v1/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const config = require('@config/config');
+
+const authRoute = require('./auth.route');
+const userRoute = require('./user.route');
+
+const router = express.Router();
+
+const defaultRoutes = [
+  {
+    path: '/auth',
+    route: authRoute,
+  },
+  {
+    path: '/users',
+    route: userRoute,
+  },
+];
+
+const devRoutes = [
+  // routes available only in development mode
+];
+
+defaultRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
+
+/* istanbul ignore next */
+if (config.env === 'development') {
+  devRoutes.forEach((route) => {
+    router.use(route.path, route.route);
+  });
+}
+
+module.exports = router;
