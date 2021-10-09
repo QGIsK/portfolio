78738b4fe65c5c1ae608af4a20b052f680df6923
Demian <Devaccdemiann@gmail.com>
2021-10-09 19:43:59 +0200

    Seperate particle component

---
+++ resources/js/components/Particles.vue
@@ -0,0 +1,54 @@
+<template>
+  <Particles id="tsparticles" class="particles" :options="options" />
+</template>
+
+<script setup>
+const options = {
+  background: {
+    color: {
+      value: 'transparent',
+    },
+  },
+  fpsLimit: 60,
+  particles: {
+    color: {
+      value: '#3A3B3C',
+    },
+    links: {
+      color: '#3A3B3C',
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: 'none',
+      enable: true,
+      outMode: 'bounce',
+      random: true,
+      speed: 2.5,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+      value: 50,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: 'circle',
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+}
+</script>
--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,89 +1,6 @@
 <template>
   <header class="bg-main">
-    <Particles
-      id="tsparticles"
-      class="particles"
-      :particlesInit="particlesInit"
-      :particlesLoaded="particlesLoaded"
-      :options="{
-        background: {
-          color: {
-            value: 'transparent',
-          },
-        },
-        fpsLimit: 60,
-        interactivity: {
-          detectsOn: 'canvas',
-          events: {
-            onClick: {
-              enable: false,
-              mode: 'push',
-            },
-            onHover: {
-              enable: false,
-              mode: 'repulse',
-            },
-            resize: true,
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40,
-            },
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: '#3A3B3C',
-          },
-          links: {
-            color: '#3A3B3C',
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: 'none',
-            enable: true,
-            outMode: 'bounce',
-            random: true,
-            speed: 2.5,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              value_area: 800,
-            },
-            value: 50,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: 'circle',
-          },
-          size: {
-            random: true,
-            value: 5,
-          },
-        },
-        detectRetina: true,
-      }"
-    />
+    <Particles />
     <div class="content h-100 w-100">
       <div class="d-block">
         <Navbar />
@@ -97,6 +14,7 @@
 
 <script setup>
 import Navbar from '../components/layout/Navbar'
+import Particles from '../components/Particles'
 </script>
 
 <style lang="scss" scoped>
