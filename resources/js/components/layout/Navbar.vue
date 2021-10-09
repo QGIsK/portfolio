645e486680c63b2b1f572e1fffb46d6b4f1fd25b
Demian <Devaccdemiann@gmail.com>
2021-10-09 20:16:34 +0200

    Seperate into portfolio item component

--- resources/js/components/layout/Navbar.vue
+++ resources/js/components/layout/Navbar.vue
@@ -1,24 +1,23 @@
 <template>
-  <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand"
-          >Demian<br />
-          Portfolio</a
-        >
-        <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" href="#contact">contact</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </div>
+  <nav class="navbar navbar-expand-lg navbar-dark mx-auto">
+    <div class="container-fluid">
+      <a class="navbar-brand"
+        >Demian's<br />
+        Portfolio</a
+      >
+      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <style lang="scss" scoped>
 .navbar {
   padding-top: 1.75rem;
+  width: 90%;
 }
 
 .navbar-brand {
--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -5,21 +5,49 @@
       <div class="d-block">
         <Navbar />
       </div>
-      <div class="container h-75 d-flex align-items-center justify-content-start">
+      <div class="h-75 d-flex align-items-center justify-content-start">
         <h3 class="text-white">I'm a <b>Web Developer</b><br />Based in <b>The Netherlands.</b></h3>
       </div>
     </div>
   </header>
+  <section id="portfolio" class="bg-secondary">
+    <div class="row h-100 justify-content-center align-items-center">
+      <PortfolioItem
+        size="col col-md-8"
+        image="https://demiann.dev/static/images/BassenMetLasse.WebP"
+        href="/"
+        header="bassen"
+        paragraph="hello"
+      />
+      <PortfolioItem
+        size="col col-md-6"
+        image="https://demiann.dev/static/images/BassenMetLasse.WebP"
+        href="/"
+        header="bassen"
+        paragraph="hello"
+      />
+      <PortfolioItem
+        size="col col-md-4"
+        image="https://demiann.dev/static/images/BassenMetLasse.WebP"
+        href="/"
+        header="bassen"
+        paragraph="hello"
+      />
+    </div>
+  </section>
 </template>
 
 <script setup>
 import Navbar from '../components/layout/Navbar'
 import Particles from '../components/Particles'
+
+import PortfolioItem from '../components/PortfolioItem'
 </script>
 
 <style lang="scss" scoped>
 header {
   height: 80vh;
+
   position: relative;
   .particles {
     position: absolute;
@@ -28,8 +56,12 @@ header {
   }
 
   .content {
+    width: 85%;
     z-index: 2;
     position: absolute;
+  }
+  h3 {
+    margin-left: 7.5vw;
   }
 }
 
@@ -38,10 +70,21 @@ header {
 }
 
 .bg-secondary {
-  background-color: #1b1a1a;
+  background-color: #1b1a1a !important;
 }
 
 h3 {
   line-height: 1.5;
+}
+
+#portfolio > .row > div:first-child {
+  margin-top: -17.5vh;
+  z-index: 10;
+}
+
+#portfolio {
+  img {
+    cursor: pointer;
+  }
 }
 </style>
