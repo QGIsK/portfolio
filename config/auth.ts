56f8a66cfac5656aae4712ce83b57a2b1b7c5fff
Demian <Devaccdemiann@gmail.com>
2021-10-11 20:14:15 +0200

    Update models

--- config/auth.ts
+++ config/auth.ts
@@ -77,7 +77,7 @@ const authConfig: AuthConfig = {
         | that time.
         |
         */
-        model: () => import('App/Models/user'),
+        model: () => import('App/Models/User'),
       },
     },
     /*
@@ -161,7 +161,7 @@ const authConfig: AuthConfig = {
         | that time.
         |
         */
-        model: () => import('App/Models/user'),
+        model: () => import('App/Models/User'),
       },
     },
   },
