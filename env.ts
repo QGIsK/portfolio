0962d0aa07e369d8614f26426aee624ccd0e42a9
Demian <Devaccdemiann@gmail.com>
2021-10-12 01:09:33 +0200

    Switch to mysql

--- env.ts
+++ env.ts
@@ -29,11 +29,11 @@ export default Env.rules({
   SMTP_USERNAME: Env.schema.string(),
   SMTP_PASSWORD: Env.schema.string(),
 
-  PG_HOST: Env.schema.string({ format: 'host' }),
-  PG_PORT: Env.schema.number(),
-  PG_USER: Env.schema.string(),
-  PG_PASSWORD: Env.schema.string(),
-  PG_DB_NAME: Env.schema.string(),
+  MYSQL_HOST: Env.schema.string({ format: 'host' }),
+  MYSQL_PORT: Env.schema.number(),
+  MYSQL_USER: Env.schema.string(),
+  MYSQL_PASSWORD: Env.schema.string(),
+  MYSQL_DB_NAME: Env.schema.string(),
 
   S3_KEY: Env.schema.string(),
   S3_SECRET: Env.schema.string(),
