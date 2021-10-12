50590ed4e077eaf4e9d8e08694630ae1e377870f
Demian <Devaccdemiann@gmail.com>
2021-10-12 15:35:35 +0200

    Remove build and public

--- build/start/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const Route_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Core/Route"));
-require("./routes/api");
-Route_1.default.on('*').render('index');
-//# sourceMappingURL=routes.js.map
