f10a8fd4c68cdd59a05cea8d63b665627dff7ab5
Demian <Devaccdemiann@gmail.com>
2021-10-19 17:14:42 +0200

    del: remove old

--- resources/js-old/app.js
+++
@@ -1,8 +0,0 @@
-import './assets/scss/app.scss'
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-
-import Particles from 'particles.vue3'
-
-createApp(App).use(Particles).use(router).mount('#app')
