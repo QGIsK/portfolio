5dcc6daa6169f7ee54be0ac347b59b5c5ef6451f
Demian <devaccdemiann@gmail.com>
2021-12-19 21:17:33 +0100

    chore: prettier

--- components/Snackbar.vue
+++ components/Snackbar.vue
@@ -1,61 +1,55 @@
 <template>
-  <div>
-    <v-card>
-      <v-snackbar
-        v-model="show"
-        :color="type"
-        right="right"
-        :timeout="timeout"
-        top="top"
-      >
-        {{ text }}
-        <v-btn color text @click="toggleSnackBar">Close</v-btn>
-      </v-snackbar>
-    </v-card>
-  </div>
+    <div>
+        <v-card>
+            <v-snackbar v-model="show" :color="type" right="right" :timeout="timeout" top="top">
+                {{ text }}
+                <v-btn color text @click="toggleSnackBar">Close</v-btn>
+            </v-snackbar>
+        </v-card>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'Snackbar',
-  components: {},
-  data() {
-    return {
-      timeout: 3000,
-    }
-  },
-  computed: {
-    show: {
-      get() {
-        return this.$store.getters.snackbarActive
-      },
-      set() {
-        const payload = {
-          type: '',
-          text: '',
-        }
-        this.$store.dispatch('toggleSnackBar', payload)
-      },
+    name: 'Snackbar',
+    components: {},
+    data() {
+        return {
+            timeout: 3000,
+        };
     },
-    text: {
-      get() {
-        return this.$store.getters.snackbarText
-      },
+    computed: {
+        show: {
+            get() {
+                return this.$store.getters.snackbarActive;
+            },
+            set() {
+                const payload = {
+                    type: '',
+                    text: '',
+                };
+                this.$store.dispatch('toggleSnackBar', payload);
+            },
+        },
+        text: {
+            get() {
+                return this.$store.getters.snackbarText;
+            },
+        },
+        type: {
+            get() {
+                return this.$store.getters.snackbarType;
+            },
+        },
     },
-    type: {
-      get() {
-        return this.$store.getters.snackbarType
-      },
+    methods: {
+        toggleSnackBar() {
+            const payload = {
+                type: '',
+                text: '',
+            };
+            this.$store.dispatch('toggleSnackBar', payload);
+        },
     },
-  },
-  methods: {
-    toggleSnackBar() {
-      const payload = {
-        type: '',
-        text: '',
-      }
-      this.$store.dispatch('toggleSnackBar', payload)
-    },
-  },
-}
+};
 </script>
