d749e6bc24473fe50f6632822326b4aa62fa25d9
Demian <devaccdemiann@gmail.com>
2021-12-19 17:25:44 +0100

    Refactor file structure

--- pages/policy/_slug.vue
+++ pages/policy/_slug.vue
@@ -17,7 +17,7 @@
 export default {
   async asyncData({ $content, params, error }) {
     const slug = params.slug || 'index'
-    const page = await $content(slug)
+    const page = await $content('policies', slug)
       .fetch()
       .catch(() => {
         error({ statusCode: 404, message: 'Page not found' })
