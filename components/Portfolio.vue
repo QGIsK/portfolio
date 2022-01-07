ba837bcd6d9d746525176b5ef2fe8d81630353db
Demian <devaccdemiann@gmail.com>
2022-01-07 12:44:58 +0100

    refactor: switch to nuxt links

--- components/Portfolio.vue
+++ components/Portfolio.vue
@@ -1,47 +1,43 @@
 <template>
     <div class="bg-dark portfolio">
         <v-container class="my-5 fill-height">
-            <v-fade-transition mode="out-in" appear>
-                <v-row class="portfolio-items">
-                    <v-col
-                        v-for="(item, index) in items"
-                        :key="index"
-                        :cols="$vuetify.breakpoint.mdAndDown ? item.size.down : item.size.up"
-                        class="mx-auto portfolio-col"
-                    >
-                        <v-card flat class="portfolio-card">
-                            <v-hover>
-                                <template #default="{hover}">
-                                    <div class="position-relative">
-                                        <v-img
-                                            :src="item.images.desktop"
-                                            class="grey darken-4"
-                                            :alt="item.title"
-                                        ></v-img>
-                                        <v-fade-transition>
-                                            <v-overlay v-if="hover" absolute color="grey darken">
-                                                <v-btn :href="item.path">More information</v-btn>
-                                            </v-overlay>
-                                        </v-fade-transition>
-                                    </div>
-                                </template>
-                            </v-hover>
-                            <v-card-title>
-                                {{ item.title }}
-                                <v-spacer></v-spacer>
-                                <i
-                                    v-for="icon in item.icons.split(' ')"
-                                    :key="icon"
-                                    :class="`fa-${icon}`"
-                                    class="fab mx-2"
-                                ></i>
-                            </v-card-title>
-                            <!-- eslint-disable-next-line vue/no-v-html -->
-                            <v-card-text v-html="item.description"></v-card-text>
-                        </v-card>
-                    </v-col>
-                </v-row>
-            </v-fade-transition>
+            <v-row class="portfolio-items">
+                <v-col
+                    v-for="(item, index) in items"
+                    :key="index"
+                    :cols="$vuetify.breakpoint.mdAndDown ? item.size.down : item.size.up"
+                    class="mx-auto portfolio-col"
+                >
+                    <v-card flat class="portfolio-card">
+                        <v-hover>
+                            <template #default="{hover}">
+                                <div class="position-relative">
+                                    <v-img :src="item.images.desktop" class="grey darken-4" :alt="item.title"></v-img>
+                                    <v-fade-transition>
+                                        <v-overlay v-if="hover" absolute color="grey darken">
+                                            <NuxtLink :to="item.path">
+                                                <v-btn>More information</v-btn>
+                                            </NuxtLink>
+                                        </v-overlay>
+                                    </v-fade-transition>
+                                </div>
+                            </template>
+                        </v-hover>
+                        <v-card-title>
+                            {{ item.title }}
+                            <v-spacer></v-spacer>
+                            <i
+                                v-for="icon in item.icons.split(' ')"
+                                :key="icon"
+                                :class="`fa-${icon}`"
+                                class="fab mx-2"
+                            ></i>
+                        </v-card-title>
+                        <!-- eslint-disable-next-line vue/no-v-html -->
+                        <v-card-text v-html="item.description"></v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
         </v-container>
     </div>
 </template>
